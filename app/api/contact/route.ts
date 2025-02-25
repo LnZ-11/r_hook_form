efa142f172ffad4ecd9986bab4850cd9030c91b9
LnZ-11 <zlyes1999@gmail.com>
2025-02-25 11:35:37 +0100

    Add contact form with email functionality and update dependencies

---
+++ app/api/contact/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_PASS);    
+
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS, 
+    },
+});
+
+export async function POST(req: Request) {
+    
+
+  try {
+    const { name ,email, message } = await req.json();
+
+    // Options de l'email
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: process.env.EMAIL_USER,
+      subject: "📩 Nouveau message depuis ton portfolio",
+      text: `De: ${name} \nEmail: ${email}\n\nMessage:\n${message}`,
+    };
+
+    // Envoi de l'email
+    await transporter.sendMail(mailOptions);
+
+    return NextResponse.json({ message: "Email envoyé avec succès" }, { status: 200 });
+  } catch (error) {
+    console.error("Erreur d'envoi:", error);
+    return NextResponse.json({ error: "Erreur lors de l'envoi" }, { status: 500 });
+  }
+}
---
+++ app/components/form.tsx
@@ -0,0 +1,102 @@
+"use client"
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+
+export default function ContactForm() {
+
+    // Define the schema for the form with zod
+    const formSchema = z.object({
+        name: z.string().nonempty(),
+        email: z.string().email("invalide format").nonempty(),
+        message: z.string().nonempty(),
+    });
+
+    // typing the form data
+    type FormData = z.infer<typeof formSchema>;
+
+    // use react-hook-form
+    const { register, handleSubmit, formState:{errors,isSubmitting},} = useForm<FormData>({resolver:zodResolver(formSchema)});
+
+    // logging results
+    const [feedback, setFeedback] = useState("");
+
+  const onSubmit = async (data: FormData) => {
+    setFeedback("");
+
+    try {
+      const res = await fetch("/api/contact", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      if (!res.ok) throw new Error("Erreur d'envoi");
+
+      setFeedback("✅ Email envoyé !");
+    } catch (error) {
+      setFeedback("❌ Erreur lors de l'envoi");
+      console.log(error);
+    }
+  };
+
+    return (
+        <form  className="flex justify-center items-center min-h-screen bg-gray-100 text-black" onSubmit={handleSubmit(onSubmit)}>
+            <div  className="bg-white shadow-lg rounded-2xl p-6 w-full max-w-sm space-y-4" >
+
+            <div className="flex-row text-center">
+            <h2 className="text-2xl font-bold text-gray-700 text-center" >Name</h2>
+            <input 
+                type="text" 
+                id="name" 
+                {...register("name")}
+                className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                placeholder="Enter your name"
+            />
+            {
+            //error message display
+            }
+            {errors.name && (<p className="text-red-500 text-sm">{errors.name.message}</p>)}
+            </div>
+
+            <div className="flex-row text-center">
+            <h2 className="text-2xl font-bold text-gray-700 text-center">Email</h2>
+            <input 
+                type="email" 
+                id="email" 
+                {...register("email",)}
+                className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                placeholder="Enter your email"
+            />
+            {errors.email && (<p className="text-red-500 text-sm">{errors.email.message}</p>)}
+            </div>
+
+            <div className="flex-row text-center">
+                <h2 className="text-2xl font-bold text-gray-700 text-center">Message</h2>
+                <textarea 
+                    id="message" 
+                    {...register("message")}
+                    className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    placeholder="Enter your message"
+                />
+                {errors.message && (<p className="text-red-500 text-sm">{errors.message.message}</p>)}
+            </div>
+
+            {/* Feedback */}
+            {feedback && <p className="text-center font-medium">{feedback}</p>}
+
+            <button
+          type="submit"
+          disabled={isSubmitting}
+          className={`w-full text-white py-3 rounded-lg font-semibold transition-all ${
+            isSubmitting ? "bg-gray-400 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-600"
+          }`}
+        >
+          {isSubmitting ? "Envoi..." : "Envoyer"}
+        </button>
+            </div>
+        </form>
+    )
+}
